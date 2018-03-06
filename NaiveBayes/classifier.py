fe20059a444b906cccf147059373e0fdb97ef985
oskam <oska.magdalena@gmail.com>
2018-03-06 15:48:17 +0100

    proper reading datasets, evaluation method, confusion martix graph

--- NaiveBayes/classifier.py
+++ NaiveBayes/classifier.py
@@ -1,48 +1,158 @@
 import numpy as np
 import pandas as pd
+import itertools
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import cm as colormap
+
+# possible data types:
+# iris
+# wine
+# glass
+# diabetes
+
+data_type = "diabetes"
+
+# Importing the datasets
+iris_path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/iris.txt"
+wine_path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/wine.txt"
+glass_path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/glass.txt"
+diabetes_path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/diabetes.txt"
 
 
+def classifier(data_type):
 
-#Importing the dataset
-path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/iris.txt"
-dataset = pd.read_csv(path)
+    X =[]
+    y = []
+
+    if data_type == "iris":
+        path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/iris.txt"
+        df = pd.read_csv(path)
+        X = df.iloc[:,:4].values
+        y = df['species'].values
+        # sns.pairplot(df, hue='species')
+        # plt.show()
+
+    elif data_type == "wine":
+        path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/wine.txt"
+        df = pd.read_csv(path)
+        class_column = 0
+        y = df[df.columns[class_column]].values
+        X = df.drop(columns=df.columns[class_column]).values
+        # sns.pairplot(df, hue='class')
+        # plt.show()
+
+    elif data_type == "glass":
+        path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/glass.txt"
+        df = pd.read_csv(path)
+        df = df.drop(columns=df.columns[0])
+        class_column = 9
+        y = df[df.columns[class_column]].values
+        X = df.drop(columns=df.columns[class_column]).values
+
+    elif data_type == "diabetes":
+        path = "/Users/oskam/PycharmProjects/classification/NaiveBayes/diabetes.txt"
+        df = pd.read_csv(path)
+        class_column = 8
+        y = df[df.columns[class_column]].values
+        X = df.drop(columns=df.columns[class_column]).values
+        # sns.pairplot(df, hue='class')
+        # plt.show()
+
+    # Splitting the dataset into the Training set and Test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+
+    # Create a new figure and set the figsize argument so we get square-ish plots of the 4 features.
+    # plt.figure(figsize=(15, 3))
+
+    # Iterate over the features, creating a subplot with a histogram for each one.
+    # for feature in range(X_train.shape[1]):
+    #     plt.subplot(1, 8, feature + 1)
+    #     plt.hist(X_train[:, feature], 20)
+    # plt.show()
+
+    # Fitting Naive Bayes Classification to the Training set with linear kernel
+    from sklearn.naive_bayes import GaussianNB, MultinomialNB
+    # nvclassifier = GaussianNB()
+    nvclassifier = MultinomialNB(alpha=1.0)
+    nvclassifier.fit(X_train, y_train)
+
+    # Predicting the Test set results
+    y_pred = nvclassifier.predict(X_test)
+
+    #---------------------------------
+
+    #lets see the actual and predicted value side by side
+    y_compare = np.vstack((y_test,y_pred)).T
+
+    #actual value on the left side and predicted value on the right hand side
+    #printing the top 5 values
+    print(y_compare[:5,:])
+
+    evaluation(y_test, y_pred, y)
+
+# def discretization():
 
 
-#looking at the first 5 values of the dataset
-# print(dataset.head())
-X = dataset.iloc[:,:4].values
-y = dataset['species'].values
+def evaluation(y_test, y_pred, y):
+    # Making the Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    # plt.figure()
+    # plot_confusion_matrix(cm, classes=[str(i) for  i in range(2)],
+    #                       title='Confusion matrix, without normalization')
 
-# Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+    #finding accuracy from the confusion matrix.
+    a = cm.shape
+    corrPred = 0
+    falsePred = 0
 
-# print(len(X_train))
-# print(len(X_test))
+    for row in range(a[0]):
+        for c in range(a[1]):
+            if row == c:
+                corrPred +=cm[row,c]
+            else:
+                falsePred += cm[row,c]
+    print('Correct predictions: ', corrPred)
+    print('False predictions', falsePred)
+    print ('Accuracy of the Naive Bayes Classification is: ', corrPred/(cm.sum()))
+    print(accuracy_score(y_test, y_pred))
 
-# Feature Scaling to bring the variable in a single scale
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Fitting Naive Bayes Classification to the Training set with linear kernel
-from sklearn.naive_bayes import GaussianNB
-nvclassifier = GaussianNB()
-nvclassifier.fit(X_train, y_train).predict(X_train)
-
-# y_pred = nvclassifier.fit(X_train, y_train).predict(X_train)
-# print(f"Number of mislabeled points out of a total {X_train.shape[0]:d} points : {(y_train != y_pred).sum():d}")
+    print(precision_score(y_test, y_pred, average=None))
+    print(recall_score(y_test, y_pred, average=None))
+    print(f1_score(y_test, y_pred, average=None))
 
 
-print(nvclassifier)
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                        ):
 
-# Predicting the Test set results
-y_pred = nvclassifier.predict(X_test)
-# print(y_pred)
+    print('Confusion matrix, without normalization')
 
-#lets see the actual and predicted value side by side
-y_compare = np.vstack((y_test,y_pred)).T
-#actual value on the left side and predicted value on the right hand side
-#printing the top 5 values
-print(y_compare[:5,:])
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest')
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+classifier(data_type)
